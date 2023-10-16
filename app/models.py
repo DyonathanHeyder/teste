9c866512ab85c7a489b4496a330c2c068b029abf
DyonathanHeyder <dyonathan2007@gmail.com>
2023-10-16 11:14:57 -0300

    Enquete, parte 1

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# Create your models here. Ao criar um modelo python3 manage.py makemigrations
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("data de publicação")
+    
+    def __str__(self):
+        return self.pub_date
+# Comando acima retorna o texto da pergunta na lista de perguntas do django
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
--- projeto/settings.py
+++ projeto/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# 'app.apps.AppConfig'ou "app", toda vez que instalar uma aplicação python3 manage.py migrate
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
